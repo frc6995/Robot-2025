1276fad0eb391a2fe4ec9671ecc8178d1b81043c
shueja-personal <j.a.shue@outlook.com>
2024-11-20 09:57:50 -0800

    Fix bug with data plumbing

--- src/main/java/frc/robot/logging/SwerveDriveStateLogger.java
+++ src/main/java/frc/robot/logging/SwerveDriveStateLogger.java
@@ -7,6 +7,7 @@ import edu.wpi.first.epilogue.logging.ClassSpecificLogger;
 import edu.wpi.first.epilogue.logging.DataLogger;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 
 @CustomLoggerFor(SwerveDriveState.class)
@@ -19,7 +20,7 @@ public class SwerveDriveStateLogger extends ClassSpecificLogger<SwerveDriveState
         dataLogger.log("cha/Pose", object.Pose, Pose2d.struct);
         dataLogger.log("mod/targets", object.ModuleTargets, SwerveModuleState.struct);
         dataLogger.log("mod/states", object.ModuleStates, SwerveModuleState.struct);
-        dataLogger.log("mod/positions", object.ModuleTargets, SwerveModuleState.struct);
+        dataLogger.log("mod/positions", object.ModulePositions, SwerveModulePosition.struct);
         dataLogger.log("cha/speed", object.Speeds, ChassisSpeeds.struct);
     }
 }
